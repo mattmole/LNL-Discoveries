867785ca6773335211d4ab83be7cf71fe3174cac
Matt Holder <mattmole@gmail.com>
2022-09-07 11:31:31 +0100

    Build for theme changes

--- docs/extra.js
+++ docs/extra.js
@@ -1,21 +1,3 @@
-/*
-$(window).on("load", function()
-  {
-      $("a").each(function()
-      {        
-        $(this).attr('style','color:#b99128 !important;');
-      });
-  });
-*/
 $(function () {$("a").each(function () {$(this).attr('style', 'color:#b99128 !important;');});});
 $(function () {$("input").each(function () {$(this).attr('style', 'color:#b99128 !important; border-color:#b99128');});});
-
 $(".wm-toctree").css("color","#b99128 !important;")
-
-
-
-//  .wm-toctree a, .wm-toctree a:visited, .wm-toctree a:hover, .wm-toctree a:focus {
-//    color: #CC342D;
-//    text-decoration: none;
-//    outline: none;
-//  }
