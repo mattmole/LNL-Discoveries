0cd026a57b85cec9528b1f13dfa39502273866db
Matt Holder <mattmole@gmail.com>
2023-04-14 19:10:21 +0100

    Removed old files from the docs directory - where the built site is stored

--- docs/extra.js
+++
@@ -1,3 +0,0 @@
-$(function () {$("a").each(function () {$(this).attr('style', 'color:#b99128 !important;');});});
-$(function () {$("input").each(function () {$(this).attr('style', 'color:#b99128 !important; border-color:#b99128');});});
-$(".wm-toctree").css("color","#b99128 !important;")
